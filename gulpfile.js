725f945e760804504aee7b98cbbb72610aacb503
JEHM94 <jesus.e.hamel@gmail.com>
2022-09-11 22:44:54 -0400

    index update

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ const notify = require('gulp-notify');
 const cache = require('gulp-cache');
 const clean = require('gulp-clean');
 const webp = require('gulp-webp');
+const plumber = require('gulp-plumber');
+const avif = require('gulp-avif');
 
 const paths = {
     scss: 'src/scss/**/*.scss',
@@ -19,48 +21,70 @@ const paths = {
     images: 'src/img/**/*'
 }
 
-function css() {
-    return src(paths.scss)
+function css(done) {
+    src(paths.scss)
         .pipe(sourcemaps.init())
+        .pipe(plumber()) // Prevents workflow from stopping
         .pipe(sass())
         .pipe(postcss([autoprefixer(), cssnano()]))
         // .pipe(postcss([autoprefixer()]))
         .pipe(sourcemaps.write('.'))
         .pipe(dest('build/css'));
+    done();
 }
 
-function javascript() {
-    return src(paths.js)
-      .pipe(sourcemaps.init())
-      .pipe(concat('bundle.js'))
-      .pipe(terser())
-      .pipe(sourcemaps.write('.'))
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./build/js'))
+function javascript(done) {
+    src(paths.js)
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js'))
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('./build/js'));
+    done();
 }
 
-function images() {
-    return src(paths.images)
+function images(done) {
+    src(paths.images)
         .pipe(cache(imagemin({ optimizationLevel: 3 })))
-        .pipe(dest('build/img'))
-        .pipe(notify({ message: 'Images Completed' }));
+        .pipe(dest('build/img'));
+    //.pipe(notify({ message: 'Images Completed' }));
+    done();
 }
 
-function versionWebp() {
-    return src(paths.images)
-        .pipe(webp())
-        .pipe(dest('build/img'))
-        .pipe(notify({ message: 'Images Completed' }));
+function convertToWebp(done) {
+    const options = {
+        quality: 50,
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(webp(options))
+        .pipe(dest('build/img'));
+
+    done();
+}
+
+function convertToAvif(done) {
+    const options = {
+        quality: 50,
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe(avif(options))
+        .pipe(dest('build/img'));
+
+    done();
 }
 
 
-function watchFiles() {
+function watchFiles(done) {
     watch(paths.scss, css);
     watch(paths.js, javascript);
     watch(paths.images, images);
-    watch(paths.images, versionWebp);
+    watch(paths.images, convertToWebp);
+    watch(paths.images, convertToAvif);
+    done();
 }
 
 exports.css = css;
+exports.images = images;
 exports.watchFiles = watchFiles;
-exports.default = parallel(css, javascript, images, versionWebp, watchFiles); 
+exports.default = parallel(css, javascript, convertToWebp, convertToAvif, watchFiles); 
