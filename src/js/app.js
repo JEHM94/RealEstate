3ff6e60a2b31b38674bc14e9e64d02118cdbee79
JEHM94 <jesus.e.hamel@gmail.com>
2022-09-17 00:15:15 -0400

    DarkMode Pref
    Set dark mode with system preference

--- src/js/app.js
+++ src/js/app.js
@@ -18,7 +18,21 @@ function responsiveNavigation() {
 
 function darkMode() {
     const buttonDarkMode = document.querySelector('.dark-mode-btn');
-    buttonDarkMode.addEventListener('click', function(){
+    const darkModePreference = window.matchMedia('(prefers-color-scheme: dark)');
+
+    darkModePreference.addEventListener('change', setDarkModePreference(darkModePreference.matches));
+
+    setDarkModePreference(darkModePreference.matches)
+
+    function setDarkModePreference(isSystemDark){
+        if (isSystemDark) {
+            document.body.classList.add('dark');
+        } else {
+            document.body.classList.remove('dark');
+        }
+    }
+
+    buttonDarkMode.addEventListener('click', function () {
         document.body.classList.toggle('dark');
     });
 }
